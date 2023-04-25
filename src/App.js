5b9d1ba1ed1325e165bcd4e0d283da0d6f8e7569
William Souza <william.h.souza.com>
2023-04-25 12:42:55 -0300

    v1.8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       
     <Banner/>
-
-
+    <Formulario/>
+    
     </div>
   );
 }
