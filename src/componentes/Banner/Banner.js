d031d368434689cd90a6fa69914323429c73ce7f
William Souza <william.h.souza.com>
2023-04-25 11:18:45 -0300

    v1.0

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner () {
+    //JSX
+    return (
+        <header className='banner'>
+             <img src="/imagens/banner.png" alt="Banner Principal da Página do Organo" />
+        </header>
+    )
+}
+
+export default Banner
